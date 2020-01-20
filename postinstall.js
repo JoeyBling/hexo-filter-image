6925a2c036c4d3d704e8c4d2f7e0009b69e453c4
zhousiwei <2434387555@qq.com>
2020-01-20 09:27:24 +0800

    Updated Log

--- postinstall.js
+++ postinstall.js
@@ -11,6 +11,7 @@ function isTrue(value) {
 // 作者
 let authorName = !!config.author.name ? config.author.name : "JoeyBling";
 let authorUrl = !!config.author.url ? config.author.url : "https://github.com/JoeyBling";
+
 let envDisable = isTrue(process.env.DISABLE_OPENCOLLECTIVE) || isTrue(process.env.CI);
 let logLevel = process.env.npm_config_loglevel;
 let logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
@@ -19,5 +20,5 @@ let logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 if (!envDisable && !logLevelDisplay) {
   console.log && console.log('Thank you for installing \u001b[35m' + config.name +
     "[" + config.version + "]" + '\u001b[0m: built with the \u001b[32m' +
-    authorName + '\u001b[0m ' + 'Hexo plugin tool (\u001b[32m' + authorUrl + '\u001b[0m)\n');
+    authorName + '\u001b[0m ' + config.description + ' (\u001b[32m' + authorUrl + '\u001b[0m)\n');
 }
